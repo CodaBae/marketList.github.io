d08d4714f915edc8f104a0920bf7196bed83ab70
Your Name <shalom.mat40@gmail.com>
2018-09-21 03:52:25 +0300

    react change

---
+++ MernR/src/App.js
@@ -0,0 +1,22 @@
+ import React, { Component } from 'react';
+
+import './styles/app.css'
+
+
+class App extends Component {
+ 
+ render(){
+  
+  return (
+      <div className="wapper-div">
+
+      </div>
+
+
+
+    );
+  }
+  }
+
+export default App;
+
--- MernR/src/index.js
+++ MernR/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
-import './style/App.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import fontAwesome from 'react-fontawesome';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
